ba42ede9707958e3fe7640ef2892bdcb0f17e941
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-05-26 00:16:45 +0600

    Added problem 2758 solution

---
+++ BEE_2758.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  float A, B;
+  double C, D;
+
+  cin >> A >> B;
+  cin >> C >> D;
+
+  cout << fixed << setprecision(6);
+  cout << "A = " << A << ", B = " << B << endl;
+  cout << "C = " << C << ", D = " << D << endl;
+
+  cout << fixed << setprecision(1);
+  cout << "A = " << A << ", B = " << B << endl;
+  cout << "C = " << C << ", D = " << D << endl;
+
+  cout << fixed << setprecision(2);
+  cout << "A = " << A << ", B = " << B << endl;
+  cout << "C = " << C << ", D = " << D << endl;
+
+  cout << fixed << setprecision(3);
+  cout << "A = " << A << ", B = " << B << endl;
+  cout << "C = " << C << ", D = " << D << endl;
+
+  cout << scientific << uppercase << setprecision(3);
+  cout << "A = " << A << ", B = " << B << endl;
+  cout << "C = " << C << ", D = " << D << endl;
+
+  cout << fixed << setprecision(0);
+  cout << "A = " << A << ", B = " << B << endl;
+  cout << "C = " << C << ", D = " << D << endl;
+
+  return 0;
+}
