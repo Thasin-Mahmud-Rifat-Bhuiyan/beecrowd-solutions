a3a258de1d9b05ad58a423520669efbe5a8ec04c
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-05-30 22:23:07 +0600

    Added problem 1005 slution

---
+++ BEE_1005.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double A, B, MEDIA;
+
+  cin >> A >> B;
+
+  MEDIA = ((A * 3.5) + (B * 7.5)) / 11.0;
+
+  cout << fixed << setprecision(5);
+  cout << "MEDIA = " << MEDIA << endl;
+
+  return 0;
+}
