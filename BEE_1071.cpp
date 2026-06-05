13fd8f63ebcd8cf4162c5a6e7b7775b4ec3a50c4
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-06-05 23:55:05 +0600

    Added problem 1071 solution

---
+++ BEE_1071.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int x, y;
+  cin >> x >> y;
+
+  int sum = 0;
+
+  if (x > y) {
+    int temp = x;
+    x = y;
+    y = temp;
+  }
+
+  for (int i = x + 1; i < y; i++) {
+    if (i % 2 != 0) {
+      sum += i;
+    }
+  }
+
+  cout << sum << endl;
+
+  return 0;
+}
