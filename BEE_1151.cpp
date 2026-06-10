7e6da3573831faf91f1a991971ac246f96e256a2
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-06-10 21:53:50 +0600

    Added problem 1151 solution

---
+++ BEE_1151.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int a = 0, b = 1;
+
+  for (int i = 0; i < n; i++) {
+    if (i > 0)
+      cout << " ";
+
+    cout << a;
+
+    int temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  cout << endl;
+
+  return 0;
+}
