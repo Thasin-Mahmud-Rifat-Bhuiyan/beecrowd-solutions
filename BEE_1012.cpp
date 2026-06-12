4daa0d4cafa5cc52af2bcadbe478e29a9f4d72fe
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-06-12 21:57:03 +0600

    Fix BEE 1012 solution

--- BEE_1012.cpp
+++ BEE_1012.cpp
@@ -9,10 +9,10 @@ int main() {
 
   cout << fixed << setprecision(3);
 
-  cout << "TRIANGULO: " << (A * C) / 2 << endl;
+  cout << "TRIANGULO: " << (A * C) / 2.0 << endl;
   cout << "CIRCULO: " << 3.14159 * C * C << endl;
-  cout << "TRAPEZIO: " << ((A + B) * C) / 2 << endl;
-  cout << "QUADRADO: " << C * C << endl;
+  cout << "TRAPEZIO: " << ((A + B) * C) / 2.0 << endl;
+  cout << "QUADRADO: " << B * B << endl;
   cout << "RETANGULO: " << A * B << endl;
 
   return 0;
