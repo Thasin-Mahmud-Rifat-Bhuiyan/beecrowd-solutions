3c18e27754b871747fd633b7a9861d75245fcbca
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-06-20 20:02:55 +0600

    Added problem 1171 solution

---
+++ BEE_1171.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  map<int, int> freq;
+
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    freq[x]++;
+  }
+
+  for (auto it : freq) {
+    cout << it.first << " aparece " << it.second << " vez(es)" << endl;
+  }
+
+  return 0;
+}
