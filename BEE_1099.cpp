bc299725b99446737e368e668d2a321eba681499
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-06-24 00:49:55 +0600

    Added problem 1099 solution

---
+++ BEE_1099.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  while (n--) {
+    int x, y;
+    cin >> x >> y;
+
+    if (x > y)
+      swap(x, y);
+
+    int sum = 0;
+
+    for (int i = x + 1; i < y; i++) {
+      if (i % 2 != 0)
+        sum += i;
+    }
+
+    cout << sum << endl;
+  }
+
+  return 0;
+}
