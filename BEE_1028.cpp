d8a4e431e233d6527c296b1913fa0f9a54a53469
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-06-25 00:25:23 +0600

    Added problem 1028 solution

---
+++ BEE_1028.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  while (n--) {
+    int a, b;
+    cin >> a >> b;
+
+    cout << __gcd(a, b) << endl;
+  }
+
+  return 0;
+}
