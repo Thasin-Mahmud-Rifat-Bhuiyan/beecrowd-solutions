7826e1c4e9eb70fbf012136cf9ac2f25ec04c4a3
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-06-30 23:08:17 +0600

    Added problem 1114 solution

---
+++ BEE_1114.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int password;
+
+  while (cin >> password) {
+    if (password == 2002) {
+      cout << "Acesso Permitido" << endl;
+      break;
+    } else {
+      cout << "Senha Invalida" << endl;
+    }
+  }
+
+  return 0;
+}
