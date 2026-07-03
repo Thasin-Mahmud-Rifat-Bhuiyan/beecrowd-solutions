1291d42fa68c9bf48f30c90036aec5512f536d58
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-07-03 23:45:36 +0600

    Added problem 1142 solution

---
+++ BEE_1142.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int x = 1;
+
+  for (int i = 0; i < n; i++) {
+    cout << x << " " << x + 1 << " " << x + 2 << " PUM" << endl;
+    x += 4;
+  }
+
+  return 0;
+}
