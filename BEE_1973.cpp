5d87c16417e40e64d30a6f82d1a9a9589708ecd1
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-08-21 00:12:34 +0600

    Replace array with vector for sheep and update logic
    Refactor sheep array to use vector and fix attacked count logic.

--- BEE_1973.cpp
+++ BEE_1973.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
 
+    vector<long long> sheep(n);
     long long total = 0;
-    int attacked = 0;
-
-    long long sheep[n];
 
     for (int i = 0; i < n; i++) {
         cin >> sheep[i];
@@ -16,6 +15,7 @@ int main() {
     }
 
     int i = 0;
+    long long attacked = 0;
 
     while (i >= 0 && i < n) {
         attacked++;
@@ -25,7 +25,7 @@ int main() {
             total--;
         }
 
-        if ((sheep[i] + 1) % 2 == 0)
+        if (sheep[i] % 2 == 0)
             i--;
         else
             i++;
