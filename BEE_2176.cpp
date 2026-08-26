46e45441ae11bd72944b7e73de5e5eb0b4031c23
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-08-27 00:46:30 +0600

    Added problem 2176 solution

---
+++ BEE_2176.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+
+  int count = 0;
+
+  for (char c : s) {
+    if (c == '1')
+      count++;
+  }
+
+  cout << s << (count % 2) << endl;
+
+  return 0;
+}
