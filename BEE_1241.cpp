b5998919b3359fa336662327284e56ce5881cd0f
Thasin-Mahmud-Rifat-Bhuiyan <tmrifat03@gmail.com>
2026-09-08 13:07:28 +0600

    Added problem 1241 solution

---
+++ BEE_1241.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  while (n--) {
+    string a, b;
+    cin >> a >> b;
+
+    if (a.size() >= b.size() && a.substr(a.size() - b.size()) == b)
+      cout << "encaixa" << endl;
+    else
+      cout << "nao encaixa" << endl;
+  }
+
+  return 0;
+}
